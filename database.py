31bcd399d72a80110740e95584b0da9270474dba
@shivamkolekar8 <shivamkolekar8@gamil.com>
2026-03-14 21:49:03 +0530

    change into database.py

--- database.py
+++ database.py
@@ -1,13 +1,15 @@
+
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./tasks.db"
+# DATABASE_URL = "mysql+pymysql://root:shivam%40123@localhost/info"
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+DATABASE_URL = os.getenv("mysql+pymysql://root:shivam%40123@localhost/info")
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
+
