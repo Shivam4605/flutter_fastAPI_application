1f260fa999b2ce7503294e2bb63e77f6116dafff
@shivamkolekar8 <shivamkolekar8@gamil.com>
2026-03-14 22:46:15 +0530

    commited

--- database.py
+++ database.py
@@ -25,20 +25,18 @@
 
 # Base = declarative_base()
 
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "mysql+pymysql://username:shivam%40123@localhost:3306/info"
-
-engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "mysql+pymysql://username:shivam%40123@db:3306/info"
 )
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
