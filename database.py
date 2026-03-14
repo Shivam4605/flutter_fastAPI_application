2043c67db7073a46f2afbc95586da0bb893313a3
@shivamkolekar8 <shivamkolekar8@gamil.com>
2026-03-14 23:55:59 +0530

    changes into the database.py

--- database.py
+++ database.py
@@ -17,7 +17,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:shivam%40123@localhost/info"
+# DATABASE_URL = "mysql+pymysql://root:shivam%40123@localhost/info"
+DATABASE_URL = "postgresql://demo_hsst_user:gQRkn7Nx1ozGsCMeOrxCPBqpFLqP5bHj@dpg-d6qqfanafjfc73b11620-a.oregon-postgres.render.com/demo_hsst"
 
 engine = create_engine(DATABASE_URL)
 
