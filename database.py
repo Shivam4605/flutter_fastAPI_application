90015664ee4564a5f14cc5771b07354248b32585
@shivamkolekar8 <shivamkolekar8@gamil.com>
2026-03-14 21:55:59 +0530

    change into database.py

--- database.py
+++ database.py
@@ -4,8 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # DATABASE_URL = "mysql+pymysql://root:shivam%40123@localhost/info"
-
-DATABASE_URL = os.getenv("mysql://root:jAQIrZGUbNUyYYBAZTYJMPoBcpeCyzHe@centerbeam.proxy.rlwy.net:49081/railway")
+DATABASE_URL = "mysql+pymysql://root:jAQIrZGUbNUyYYBAZTYJMPoBcpeCyzHe@centerbeam.proxy.rlwy.net:49081/railway"
 
 engine = create_engine(DATABASE_URL)
 
