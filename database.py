6ab093247f4895e7a7265730098f9ebfb14e8c4c
@shivamkolekar8 <shivamkolekar8@gamil.com>
2026-03-14 20:31:05 +0530

    change into database.py

--- database.py
+++ database.py
@@ -19,7 +19,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 # DATABASE_URL = "mysql+pymysql://root:shivam%40123@localhost/info"
 
-DATABASE_URL = "mysql+pymysql://user:shivam%40123@host:3306/database"
+DATABASE_URL = "mysql://root:jAQIrZGUbNUyYYBAZTYJMPoBcpeCyzHe@centerbeam.proxy.rlwy.net:49081/railway"
 
 engine = create_engine(DATABASE_URL)
 
